5366ee05d8b76ab684bee66f3121a3dc0bd7c0c3
Admin <alex.r@ramotion.agency>
2021-01-05 21:56:42 +0300

    monday test complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Display from "./components/Display";
+import Button from "./components/button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const MAX_VALUE: number = 5;
+    const MIN_VALUE: number = 0;
+    const [counter, setCounter] = useState<number>(MIN_VALUE);
+
+    const increment = () => {
+        if (counter === MAX_VALUE) return;
+        setCounter(counter + 1)
+    };
+
+    const reset = () => setCounter(MIN_VALUE);
+
+    return (
+        <div className="App">
+            <div className="wrapper">
+                <Display count={counter} />
+                <div className="buttons">
+                    <Button
+                        name={'increment'}
+                        isDisabled={counter === MAX_VALUE}
+                        callBack={increment} />
+
+                    <Button
+                        name={'reset'}
+                        isDisabled={counter === MIN_VALUE}
+                        callBack={reset} />
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
